e2be40c71dded5e479dc99a6c8ee2e29cd112f2f
viniciusfl <viniciushenrique2301@gmail.com>
2022-10-17 11:50:24 -0300

    organização do código e mudança do idioma usado no código

--- projeto_base/projeto_base.ino
+++ projeto_base/projeto_base.ino
@@ -7,6 +7,11 @@
     bancoA.timestamp
     bancoB.timestamp
 */
+#define DEBUG 1
+
+#define RETRY_TIME 60000
+#define DOWNLOAD_INTERVAL 15000
+
 #include <SPI.h>
 #include <Ethernet.h>
 #include <SD.h>
@@ -14,7 +19,6 @@
 #include <EthernetUdp.h>
 #include <TimeLib.h>
 
-// ---------------------- UDP ---------------------
 /*
 unsigned int localPort = 8888;       // local port to listen for UDP packets
 
@@ -25,21 +29,14 @@ const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of th
 byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
 
 EthernetUDP Udp; // A UDP instance to let us send and receive packets over UDP
+
 */
 
-// --------------------- relógio ---------------------
+
+File file;
+
 RTC_DS1307 rtc;
 
-unsigned long milisPrevia = 0;
-
-/*
-char daysOfTheWeek[7][12] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
-
-
-
-unsigned long intervalo = 30000; */
-
-// ---------------------- ethernet client ---------------------
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
@@ -47,156 +44,189 @@ IPAddress server(10, 0, 2, 113);
 
 EthernetClient client;
 
-File arquivo;
-
-bool lendoDoCliente = false;
-
-bool headerHTTP = false;
-
-bool banco = 0; // 0 se o banco A for o mais antigo, 1 se o banco B for o mais antigo.
-
 void setup() {
   delay(150);
   Serial.begin(9600);
+
+  // Wait until serial connection is established
    while (!Serial) {
     ; 
   }
   
-  // inicializa internet
+  // Initialize ethernet
   Serial.println(Ethernet.begin(mac));
   
-  // inicializa relógio
+  // Initialize rtc 
   Serial.println(rtc.begin());
 
-  if (! rtc.isrunning()) { // se o relógio não estiver funcionando, atualiza o horário uma vez
+  if (! rtc.isrunning()) { // if the clock wasn't running, adjust the time once 
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
-  // inicia leitor de cartão SD
+  // Initialize SD card module
   Serial.println(SD.begin(4));
 
-  // verifico quem é o bd mais novo
-  arquivo = SD.open("A.txt");
-  long tempoA = arquivo.parseInt();
-  arquivo.close();
-
-  arquivo = SD.open("B.txt");
-  long tempoB = arquivo.parseInt();
-  arquivo.close();
-
-  if(tempoA < tempoB){
-    banco = 1;
-  }else{
-    banco = 0;
-  }
+  // Firstly, we must know the newest DB
+  chooseCurrentDB();
 }
 
 void loop() {
+
+  // Update the oldest DB
+  dbMaintenance();
+}
+
+
+String dbfiles[] = {"bancoA.txt", "bancoB.txt"};
+String timestampfiles[] = {"A.txt", "B.txt"};
+char currentDB = -1; 
+unsigned long lastDownloadTime = 0;
+bool downloading = false; // Is there an ongoing download?
+bool headerDone = false; // Did we read the header already?
+String line = "";
+char newDB; 
+inline void dbMaintenance() {
   DateTime now = rtc.now();
-  unsigned long milisAtual = millis(); // milisegundos desde que o arduino ligou (obs: ele pode guardar "acho" que uns 40 dias!!)
-
-  /*
-  if(Serial.available() > 0){ // se alguém mandou algo pelo serial
-    int num = toInt(Serial.readString());
-    verificaIdNoBanco(num);
-  }*/
-  
-  if(lendoDoCliente){ // se ainda não terminei de ler o conteúdo que o cliente mandou
-    Serial.println(F("estou  lendo mais uma linha do cliente..."));
-    lendoDoCliente = leCliente();
-    if(!lendoDoCliente){ // serve para debuggar
-      headerHTTP = false; // reseta a variável que diz se passamos do header ou não
-      if(banco){ 
-        banco = 0;
-      }else{
-        banco = 1;
-      }
-      Serial.println(F("parei de me comunicar com o cliente. devo ter lido tudo já"));
+    // We start a download only if we are not already downloading
+    if (!downloading) {
+        // millis() wraps every ~49 days, but I *think*
+        // wrapping does not cause problems
+        // TODO: we might prefer to use dates,
+        //       such as "at midnight"
+        if (millis() - lastDownloadTime > DOWNLOAD_INTERVAL) startDownload();
+        return;
     }
-  }else if (milisAtual - milisPrevia > 15000) { // se já se passaram 15 segundos eu tento atualizar o banco de dados
-    chamaCliente(now);
-    Serial.println(F("vou começar a ler do cliente..."));
-    lendoDoCliente = true;
-    milisPrevia = milisAtual;
-  }  
+
+    // If we did not return above, we are downloading
+    if (!client.connected() && !client.available()) {
+        finishDownload();
+        return;
+    }
+
+    // If we did not disconnect above, we are connected
+    processDownload();
 }
 
 
-void chamaCliente(DateTime now){
-  if (client.connect(server, 80)) {
-      Serial.println("conectado servidor.");
-    }else{
-      Serial.println("conexão servidor failed");  
-    }
-    
-  // HTTP request:
-  //client.println("GET /localhost/index/arduino.xml HTTP/1.0");
-  client.println("GET /arduino.txt HTTP/1.1");
-  client.println("Host: 10.0.2.113");
-  //client.println("Connection: close");
-  client.println();
+inline void startDownload() {
+    client.connect(server, 80);
 
-  /*
-   * Já que no setup eu vejo quem é o banco mais novo, então eu não preciso procurar de novo. Basta atualizar o tempo do mais antigo
-   */
-  long unixTime = now.unixtime();
-  if(banco){ // se o banco B é o mais novo eu vou excluir o banco A e o datetime de A, e atualizar o datetime de A
-    Serial.println("banco A é o mais antigo... apagando");
-    SD.remove("A.txt");
-    SD.remove("bancoA.txt");  
-    arquivo = SD.open("A.txt", FILE_WRITE);
-    arquivo.println(unixTime);
-    arquivo.close();
-  }else{ // se o banco A é o mais novo, ...
-    Serial.println("banco B é o mais antigo... apagando");
-    SD.remove("B.txt");
-    SD.remove("bancoB.txt");  
-    arquivo = SD.open("B.txt", FILE_WRITE);
-    arquivo.println(unixTime);
-    arquivo.close();
-  }
+#   ifdef DEBUG
+      if (client.connected()) {
+          Serial.println(F("Connected to server."));
+      } else {
+          Serial.println(F("Connection to server failed."));
+      };
+#   endif
+
+    // If connection failed, pretend nothing
+    // ever happened and try again later
+    if (!client.connected()) {
+        lastDownloadTime = lastDownloadTime + RETRY_TIME;
+        client.stop();
+        return;
+    }
+
+    downloading = true;
+    headerDone = false;
+    line = "";
+
+    client.println(F("GET /arduino.txt HTTP/1.1"));
+    client.println(F("Host: 10.0.2.113"));
+    client.println();
+
+    
+    if (currentDB == 0) {newDB = 1;}
+    else {newDB = 0;};
+
+    SD.remove(dbfiles[newDB]);
+    SD.remove(timestampfiles[newDB]);
+    
+#   ifdef DEBUG
+      Serial.println("Updating DB " + dbfiles[newDB]);
+#   endif
+  
+    file = SD.open(dbfiles[newDB], FILE_WRITE);
+    
 }
 
-bool leCliente(){
-  /* função que recebe a resposta do http request, ignora o response header, lê uma linha que o cliente mandou e escreve ela no arquivo
-   *  OBS: fazer com char depois pois é mais rápido e flexível.
-   */
-   /*
-  if(banco){
-    arquivo = SD.open("bancoA.txt", FILE_WRITE);
-  }else{
-    arquivo = SD.open("bancoB.txt", FILE_WRITE);
-  }*/
-  
-  bool liPalavra = false;
-  String palavra = "";
-  char c;
-  // começa a ler o conteúdo do cliente
-  while(!liPalavra && client.connected() && client.available()){
-    c = client.read();
-    palavra = palavra + String(c);
-    if (String(c) == "\n") {
-      if(headerHTTP){
-        arquivo.println(palavra);
-        Serial.println(palavra);
-      }
-      if(!headerHTTP && palavra == "\r\n"){
-        headerHTTP = true;
-        arquivo.println("");
-        Serial.println("header...");
-      }
-      palavra = "";
-      liPalavra = true;
+bool beginningOfLine = true;
+char previous;
+inline void processDownload() {
+    if (!client.available()) return;
+    char c = client.read();
+    if (headerDone) {
+        if (c == '\r') return;
+        if (c == '\n') {
+            file.println(line);
+            Serial.println(line);
+            line = "";
+            return;
+        }else{
+          line = line + String(c); // FIXME: this is very expensive!
+        }
+    } else {
+        if (c == '\n') {
+            if (beginningOfLine and previous == '\r') {
+                headerDone =  true;
+                line = "";
+            } else {
+                previous = 0;
+            }
+            beginningOfLine = true;
+        } else {
+            line = line + String(c); // FIXME: this is very expensive!
+            previous = c;
+            if (c != '\r') beginningOfLine = false;
+        }
     }
-    
-  }
-  Serial.println();
-  arquivo.close();
-  if(client.connected() && client.available()){ // se o cliente está conectado e mandando coisas ainda mesmo depois de lermos uma linha
-    return true; 
-  }else{ // lemos tudo, podemos desconectar do cliente. 
-    Serial.println("lemos tudo do cliente...");
+}
+
+inline void finishDownload() {
+#   ifdef DEBUG
+      Serial.println(F("Disconnecting from server."));
+#   endif
+    file.close();
     client.stop();
-    return false;
-  }
+    downloading = false;
+    // FIXME: we should only save the timestamp etc.
+    //        if the download was successful
+#   ifdef DEBUG
+      Serial.println("Writing new timestamp on " + timestampfiles[newDB]);
+#   endif
+    file = SD.open(timestampfiles[newDB], FILE_WRITE);
+    file.println(rtc.now().unixtime());
+    file.close();
+    //chooseCurrentDB();
+    currentDB = newDB;
+    lastDownloadTime = millis();
+#   ifdef DEBUG
+      Serial.println("Finished...");
+#   endif
+}
+
+char chooseCurrentDB() {
+    currentDB = -1; // invalid
+    long previousBestTime = -1;
+    for (char i = 0; i < 2; ++i) { // 2 is the number of DBs
+        File f = SD.open(timestampfiles[i]);
+        if (!f) {
+#           ifdef DEBUG
+              Serial.println("DB " + dbfiles[i] + " not available");
+#           endif
+        } else {
+            long t = f.parseInt(); // If reading fails, this returns 0
+            if (t >= 0 and t > previousBestTime) {
+                previousBestTime = t;
+                currentDB = i;
+            }
+        }
+        f.close();
+    }
+#   ifdef DEBUG
+      Serial.println("Choosing DB " + dbfiles[currentDB] + " as actual DB.");
+      if (currentDB < 0) {
+          Serial.println(F("No DB available!"));
+      }
+#   endif
 }
