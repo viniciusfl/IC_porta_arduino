7dd75f566aca500631783683cff5e5c54fdf0fcf
viniciusfl <viniciushenrique2301@gmail.com>
2022-11-24 15:29:03 -0300

    comment deleted

--- 221120-102033-esp32doit-devkit-v1/include/cardReader.h
+++ 221120-102033-esp32doit-devkit-v1/include/cardReader.h
@@ -1,9 +1,6 @@
 #include <Wiegand.h>
 #include <common.h>
 
-// These are the pins connected to the Wiegand D0 and D1 signals.
-// Ensure your board supports external Interruptions on these pins
-
 void processCardData(uint8_t* data, uint8_t bits);
 
 void dbStartSearch1(uint8_t* data, uint8_t bits, const char* message);
