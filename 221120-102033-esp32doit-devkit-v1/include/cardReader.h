4ea24f04e1fa4929f2ccf0d420c5d535a5294951
viniciusfl <viniciushenrique2301@gmail.com>
2022-12-01 14:23:34 -0300

    remove 'private' declarations from .h

--- 221120-102033-esp32doit-devkit-v1/include/cardReader.h
+++ 221120-102033-esp32doit-devkit-v1/include/cardReader.h
@@ -1,18 +1,10 @@
-#include <Wiegand.h>
+#ifndef CARD_READER_H
+#define CARD_READER_H
+
 #include <common.h>
-
-void processCardData(uint8_t* data, uint8_t bits);
-
-void dbStartSearch1(uint8_t* data, uint8_t bits, const char* message);
-
-void dbStartSearch2(uint8_t* data, uint8_t bits, const char* message);
-
-void pinStateChanged();
-
-void stateChanged(bool plugged, const char* message);
-
-void receivedDataError(Wiegand::DataError error, uint8_t* rawData, uint8_t rawBits, const char* message);
 
 void initCardReader();
 
 void cardMaintenance();
+
+#endif
--- 221120-102033-esp32doit-devkit-v1/include/networkConnection.h
+++ 221120-102033-esp32doit-devkit-v1/include/networkConnection.h
@@ -1,20 +1,10 @@
-#include <WiFi.h>
+#ifndef NETWORK_CONNECTION_H
+#define NETWORK_CONNECTION_H
+
 #include <common.h>
-
-#define CHECK_NET_INTERVAL 30000 // 30s
-
-#define RECONNECT_INTERVAL 30000 // 30s
-
-extern unsigned long lastNetCheck;
-
-extern unsigned long lastReconnectAttempt;
+#include <WiFi.h>
 
 void WiFiInit();
 
 void printNetStatus();
-
-void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info);
-
-void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info);
-
-void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info);
+#endif
