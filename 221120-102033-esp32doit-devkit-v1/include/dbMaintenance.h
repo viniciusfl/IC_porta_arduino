668f241974d293fa9d2bbe0269e56b57cb5019fd
viniciusfl <viniciushenrique2301@gmail.com>
2022-12-01 14:23:57 -0300

    split public/private class members

--- 221120-102033-esp32doit-devkit-v1/include/dbMaintenance.h
+++ 221120-102033-esp32doit-devkit-v1/include/dbMaintenance.h
@@ -1,28 +1,23 @@
+#ifndef DB_MAITENANCE
+#define DB_MAITENANCE
+
+#include <WiFi.h>
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
 #include <sqlite3.h>
 #include <networkConnection.h>
 #include <common.h>
+#include <timeKeeping.h>
 
 class dataBase{
     public:
-        dataBase();
         void initDataBase();
-        void dbMaintenance();
-        void startDownload();
-        void finishDownload();
-        void processDownload();
+        void dbMaintenance(DateTime moment);
         void dbStartSearch(uint8_t* data, uint8_t bits, const char* message);
-        void chooseCurrentDB();
-        void resetTimestampFiles();
-        int openDb(const char *filename) ;
-        int exec(const char *sql);
-        void insert(char *element);
-        void close();
-        bool search();
+
+    private:
         sqlite3 *db;
-        File arquivo;
         const char *dbNames[100] = {"bancoA", "bancoB"};
         const char *timestampfiles[100] = {"/TSA.TXT", "/TSB.TXT"};
         int currentDB = -1; // invalid
@@ -35,6 +30,20 @@ class dataBase{
         char previous;
         char *zErrMsg = 0;
         const char* data = "Callback function called";
+
+        void startDownload();
+        void finishDownload();
+        void processDownload();
+        void chooseCurrentDB();
+        void resetTimestampFiles();
+        void generateLog(DateTime moment, unsigned long int input);
+        int openDb(const char *filename) ;
+        void close();
+        int exec(const char *sql);
+        void insert(char *element);
+        bool search(DateTime moment);
 };
 
 static int callback(void *data, int argc, char **argv, char **azColName);
+
+#endif
--- 221120-102033-esp32doit-devkit-v1/include/timeKeeping.h
+++ 221120-102033-esp32doit-devkit-v1/include/timeKeeping.h
@@ -1,33 +1,25 @@
+#ifndef TIME_KEEPING_H
+#define TIME_KEEPING_H
+
 #include <common.h>
 #include <SPI.h>
 #include <stdlib.h>
 #include <WiFiUdp.h>
 #include <RTClib.h>
-
-#define LOCALPORT 8888       // local port to listen for UDP packets
-
-#define READJUST_CLOCK_INTERVAL 10000 // 10s, just for testing; a good
-                                      // value is 10800 (3 hours)
-
-#define NTP_WAIT_TIMEOUT 10000 // 10s, waaay more than enough
-
-#define TIME_SERVER "a.ntp.br"
-
-#define NTP_PACKET_SIZE 48 // NTP time stamp is in the first 48 bytes of the message
-
+#include <time.h>
 
 class RTC{
     public:
         RTC();
         void initRTC();
         void checkRTCsync();
-        void processNTPreply();
-        void sendNTPquery();
+        void updateRTC();
         void printDate(DateTime moment);
-        RTC_DS1307 rtc;
-        bool waitingForNTPreply;
-        unsigned long lastClockAdjustment; // variable that holds the last time we adjusted the clock
-        byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-        WiFiUDP udp; // A UDP instance to let us send and receive packets over UDP
+        unsigned long int unixTime();
 
+    private:
+        RTC_DS1307 rtc;
+        unsigned long lastClockAdjustment; // variable that holds the last time we adjusted the clock
 };
+
+#endif
