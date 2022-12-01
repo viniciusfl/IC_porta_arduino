0dd5f30f30bace0aa10e52b218aff3e79df930a5
viniciusfl <viniciushenrique2301@gmail.com>
2022-12-01 14:25:40 -0300

    move #define's from .h to cpp and use internal ntp function to update rtc

--- 221120-102033-esp32doit-devkit-v1/src/timeKeeping.cpp
+++ 221120-102033-esp32doit-devkit-v1/src/timeKeeping.cpp
@@ -1,4 +1,21 @@
 #include <../include/timeKeeping.h>
+#define DEBUGRTC
+
+#define LOCALPORT 8888       // local port to listen for UDP packets
+
+#define READJUST_CLOCK_INTERVAL 60000 // 10s, just for testing; a good
+                                      // value is 10800 (3 hours)
+
+#define NTP_WAIT_TIMEOUT 10000 // 10s, waaay more than enough
+
+#define TIME_SERVER "a.ntp.br"
+
+#define NTP_PACKET_SIZE 48 // NTP time stamp is in the first 48 bytes of the message
+
+
+const char* ntpServer = "a.ntp.br";
+const long  gmtOffset_sec = 0;
+const int   daylightOffset_sec = -3600*3;
 
 // This should be called from setup()
 RTC::RTC(){
@@ -12,6 +29,9 @@ void RTC::initRTC(){
         Serial.flush();
         while (true) delay(10);
     }
+
+    // esp32 internal rtc
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 
     // The RTC may be started and stopped by setting bit 7 of register 0
     // to 0 or 1 respectively. Register 0 is the seconds register, which
@@ -35,14 +55,12 @@ void RTC::initRTC(){
 
 // This should be called from loop()
 void RTC::checkRTCsync() {
-    if (waitingForNTPreply) {
-        processNTPreply();
-    } else if (currentMillis - lastClockAdjustment > 5000) {
+    if (currentMillis - lastClockAdjustment > READJUST_CLOCK_INTERVAL) {
         // Actually, this is the time of the last *attempt* to adjust
         // the clock, but that's ok: If it fails, we do nothing special,
         // just wait for READJUST_CLOCK_INTERVAL again.
         lastClockAdjustment = currentMillis;
-        sendNTPquery(); // send an NTP packet to a time server
+        updateRTC(); 
     }
 }
 
@@ -70,41 +88,18 @@ void RTC::printDate(DateTime moment){
     Serial.println(" UTC"); // Let's always use UTC
 }
 
-
-// FIXME there is a remote chance someone will spoof a packet and alter
-//       the clock. Is this relevant? Does the NTP library handle this?
-void RTC::processNTPreply(){
-    // Timeout
-    
-    if (currentMillis - lastClockAdjustment > NTP_WAIT_TIMEOUT) {
-        udp.stop();
-        waitingForNTPreply = false;
+void RTC::updateRTC(){
+    time_t now;
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+        Serial.println("Failed to obtain time");
         return;
     }
+    time(&now);
 
-    if (!udp.parsePacket()) return; // still waiting...
-
-    // We've received a packet, read the data from it
-    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-    udp.stop();
-    waitingForNTPreply = false;
-
-    // the timestamp starts at byte 40 of the received packet and is
-    // four bytes, or two words, long. First, extract the two words:
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-
-    // combine the four bytes (two words) into a long integer;
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long nettime = highWord << 16 | lowWord;
-
-    // now convert NTP time into unix time (seconds since
-    // Jan 1 1970 UTC). In seconds, the difference is 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    nettime = nettime - seventyYears;
-
+    unsigned long nettime = now + daylightOffset_sec;
     unsigned long rtctime = rtc.now().unixtime();
-
+#   ifdef DEBUGRTC
     Serial.print("NTP date: ");
     Serial.println(nettime);
     printDate(DateTime(nettime));
@@ -118,32 +113,9 @@ void RTC::processNTPreply(){
         Serial.println("Using NTP to update RTC time");
         rtc.adjust(DateTime(nettime));
     }
-    
+#   endif
 }
 
-// send an NTP request to the time server
-void RTC::sendNTPquery(){
-    // set all bytes in the buffer to 0
-    memset(packetBuffer, 0, NTP_PACKET_SIZE);
-
-    // Initialize values needed to form NTP request
-    // (see URL above for details on the packets)
-    // FIXME which URL?
-    packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-    packetBuffer[1] = 0;     // Stratum, or type of clock
-    packetBuffer[2] = 6;     // Polling Interval
-    packetBuffer[3] = 0xEC;  // Peer Clock Precision
-    // 8 bytes of zero for Root Delay & Root Dispersion
-    packetBuffer[12]  = 49;
-    packetBuffer[13]  = 0x4E;
-    packetBuffer[14]  = 49;
-    packetBuffer[15]  = 52;
-
-    // all NTP fields have been given values, now
-    // you can send a packet requesting a timestamp:
-    udp.begin(LOCALPORT); // prepare the connection to the NTP server
-    udp.beginPacket(TIME_SERVER, 123); // NTP requests are to port 123
-    udp.write(packetBuffer, NTP_PACKET_SIZE);
-    udp.endPacket();
-    waitingForNTPreply = true;
+unsigned long int RTC::unixTime(){
+    return rtc.now().unixtime();
 }
