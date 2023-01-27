0040cbac3ee6cf766b0eeb88f10138dcad718201
Nelson Lago <lago@ime.usp.br>
2023-01-27 15:22:24 -0300

    Remove commented code

--- 221120-102033-esp32doit-devkit-v1/src/cardreader.cpp
+++ 221120-102033-esp32doit-devkit-v1/src/cardreader.cpp
@@ -133,16 +133,6 @@ void IRAM_ATTR setInternal1PinState() {
 }
 
 
-/*
-external.setPin0State(digitalRead(EXTERNAL_D0));
-
-external.setPin1State(digitalRead(EXTERNAL_D1));
-
-internal.setPin0State(digitalRead(INTERNAL_D0));
-
-internal.setPin1State(digitalRead(INTERNAL_D1));
-*/
-
     // This should be called from setup()
     void initCardReaders() {
 
