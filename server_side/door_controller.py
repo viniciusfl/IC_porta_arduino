638fdfd60da76a0afeff34d24b0952bc3ae2843c
Nelson Lago <lago@ime.usp.br>
2024-02-16 18:55:51 -0300

    Fix typo

--- server_side/door_controller.py
+++ server_side/door_controller.py
@@ -100,7 +100,7 @@ class OurMQTT():
 
                 if topic == "commands":
                     os.remove(filename)
-                print("Published with sucess\n") if result[0] == 0 else print("Publish failed!\n")
+                print("Published with success\n") if result[0] == 0 else print("Publish failed!\n")
         except:
             print(f"File {filename} not found!")
         
